b25ad3fd0afad88edd5864856c41b5f7c3b89439
Bryan A Quero <bryanq2008@live.com>
2022-12-20 17:10:37 -0500

    update #7

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,3 +1,35 @@
-let weather = {
-    "apiKey": YOUR API
+const weather = {
+    "apiKey":"105179e2ff6654aeb72f470eca7ba2d8",
+    fetchWeather: function(city){
+        fetch( "https://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        + "&units=standard&appid="
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = 
+        "Weather in " 
+        + name;
+        document.querySelector(".icon").src 
+        = "https://openweathermap.org/img/wn/"
+        + icon + ".png";
+        document.querySelector(".description").innerText 
+        =   description;
+        document.querySelector(".temp").innerText
+         = temp + "°C";
+        document.querySelector(".humidity").innerText 
+        = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText 
+        = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading"); 
+    },
+
 }
